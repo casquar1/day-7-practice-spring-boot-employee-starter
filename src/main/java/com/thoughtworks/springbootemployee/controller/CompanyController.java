01d69b267dc07cf2f80c166492106c66113c3723
Arianne <chiescake11@gmail.com>
2023-08-22 22:01:55 +0800

    feat: implement addACompany

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -36,4 +37,11 @@ public class CompanyController {
     public List<Company> listCompanyByPage(@RequestParam Long pageNumber, @RequestParam Long pageSize) {
         return companyRepository.listCompanyByPage(pageNumber, pageSize);
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company addACompany(@RequestBody Company company) {
+        return companyRepository.save(company);
+    }
+
 }
--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyRepository.java
@@ -10,6 +10,8 @@ import java.util.stream.Collectors;
 @Repository
 public class CompanyRepository {
     private static final List<Company> companies = new ArrayList<>();
+    private static final long START_ID_MINUS_ONE = 0L;
+    private static final long ID_INCREMENT = 1;
     @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -44,5 +46,19 @@ public class CompanyRepository {
                 .skip((pageNumber - 1) * pageSize)
                 .limit(pageSize)
                 .collect(Collectors.toList());
+    }
+
+    public Company save(Company company) {
+        Long id = generateNextId();
+        Company toBeSaveCompany = new Company(id, company.getName());
+        companies.add(toBeSaveCompany);
+        return toBeSaveCompany;
+    }
+
+    private Long generateNextId() {
+        return companies.stream()
+                .mapToLong(Company::getId)
+                .max()
+                .orElse(START_ID_MINUS_ONE) + ID_INCREMENT;
     }
 }
